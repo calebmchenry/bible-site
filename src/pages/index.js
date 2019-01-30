0dd49d54859e29fa17f0426a00f388579bca8744
calebmchenry <calebmchenry@gmail.com>
2019-01-30 17:04:16 -0500

    finished header styles for now

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,19 @@
-import React from 'react'
+import React from 'react';
 
-import Layout from '../components/layout/layout'
-import Image from '../components/image'
-import SEO from '../components/seo'
+import Layout from '../components/layout/layout';
+import Image from '../components/image';
+import SEO from '../components/seo';
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`bible`, `christian`, `truth`]} />
     <h1>Hello people,</h1>
     <p>Welcome to this bible site.</p>
-    <p>Now go build something great.</p>
+    <p>It is currently under construction.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
   </Layout>
-)
+);
 
-export default IndexPage
+export default IndexPage;
