1b105e4002128f27d182f8856f3ff43590006f16
calebmchenry <calebmchenry@gmail.com>
2019-01-30 22:25:13 -0500

    added rotating title for home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,17 @@ import SEO from '../components/seo';
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`bible`, `christian`, `truth`]} />
-    <h1>Hello people,</h1>
+    <h1>
+      The Bible is
+      <div className="slidingVertical">
+        <span>Help</span>
+        <span>Love</span>
+        <span>Wisdom</span>
+        <span>Truth</span>
+        <span>Everything</span>
+      </div>
+    </h1>
+    <h1>for those who apply it.</h1>
     <p>Welcome to this bible site.</p>
     <p>It is currently under construction.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
